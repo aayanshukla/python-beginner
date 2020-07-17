19b0d7ac39ae4667580bef3d2f7cf6b09d060bfb
Aayan Shukla <aayan.as@outlook.com>
2020-07-18 00:18:12 +0530

    game added

--- test.py
+++ test.py
@@ -1,3 +1,20 @@
 print('Hello user')
 name=input('Enter your name--> ')
-print(f"Hello{name}")
+print(f"Hello {name}")
+game=input('Would you like to play game? [y/n] -->')
+secret_number = 7
+if game.upper() == "Y" :
+    print('Guess the secret number!!')
+    guess_count=0
+    guess_limit=3
+    while guess_count < guess_limit:
+        guess = int(input('Guess: '))
+        guess_count += 1
+        if guess == secret_number:
+            print('You won!!')
+            break
+    else:
+        print('You lost!!')
+else:
+    print('NO GAME NO PROBLEM!!')
+    
