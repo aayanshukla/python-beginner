6beba2a09ed41641d406ab339fac0fa56bec79c9
Aayan Shukla <aayan.as@outlook.com>
2020-07-18 11:51:42 +0530

    added try and except command for 3.2

--- py4e.py
+++ py4e.py
@@ -1,8 +1,11 @@
 print('Hello Aayan')
 hour=input('Enter hour: ')
 rate=input('Enter rate: ')
-fh=float(hour)
-fr=float(rate)
+try:
+    fh=float(hour)
+    fr=float(rate)
+except:
+    print('Enter a numeric value')
 reg= fh*fr
 if fh > 40:
     ovtp = (fh-40)*(fr*0.5)
