0f5fd32aaa8a7c5db3b8bcebadbfbdef560dad15
Aayan Shukla <aayan.as@outlook.com>
2020-07-18 11:31:29 +0530

    added overtime pay exerise 3.1 from coursera week 5

--- py4e.py
+++ py4e.py
@@ -1,1 +1,12 @@
- print('Hello Aayan')
+print('Hello Aayan')
+hour=input('Enter hour: ')
+rate=input('Enter rate: ')
+fh=float(hour)
+fr=float(rate)
+reg= fh*fr
+if fh > 40:
+    ovtp = (fh-40)*(fr*0.5)
+    pay = reg+ovtp
+    print('Pay:',pay)
+else:
+    print('Pay:',pay)
