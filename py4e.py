731273eb308eb258abe51f037bf4c6fa4368349d
Aayan Shukla <aayan.as@outlook.com>
2020-08-11 13:03:06 +0530

    suicide

--- py4e.py
+++
@@ -1,9 +0,0 @@
-cel=int(input('Enter tempreture--> '))
-print('Press [C] for Celsuis or [F]arhenheit')
-convt_fact=input('Would you like to convert the following tempreture in [C]elsuis or [F]arhenheit? ')
-if convt_fact=="f".lower():
-    tempreture=(9/5*cel)+32
-    print(f'Tempreture in Farhenheit is {tempreture} Farhenheit')
-else:
-    tempreture=(cel-32)*5/9
-    print(f'Tempreture in Celsuis is {tempreture} Celsuis')
